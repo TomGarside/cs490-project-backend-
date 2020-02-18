bae41ccb5f350f0314d596adbf7d0b3ffa8541f6
Thomas Garside <tg253@afsconnect1.njit.edu>
2020-02-18 18:53:36 -0500

    cleaned up alpha

--- userservice.php
+++ userservice.php
@@ -1,11 +1,10 @@
 <?php 
-// uncomment for debug 
-//ini_set('display_errors', 1); error_reporting(E_ALL);
 
-
+// handles http connection and initializes DB object 
 class httpHandler {
 
-  public function __construct($fileName){
+    // load DB creds from file and initialize DB object 
+    public function __construct($fileName){
         // load db credentials from protected secrets file 
   	$secretsFile = fopen($fileName,"r") or die ("Unable to open file! " . $fileName);
 
@@ -16,65 +15,42 @@ class httpHandler {
         fclose($secretsFile);   
       
         $this->db = new dbConnect($servername, $username, $password); 
+    } 
 
-  } 
+    // handles web requests 
+    public function handleRequest($request, $method, $body){
 
- // handles web requests 
- public function handleRequest($request, $method, $body){
-
-    switch($method){
-    
-    case 'get':
-          header('Content-type: text/plain');
-          $resp = $this->db->getHash(htmlspecialchars($_GET["password"])); 
-          echo $resp;
-          break;
+        switch($method){
  
-    case 'post':
-          $request = json_decode($body);
-          header('Content-type: application/json');
-          echo "{\"ValidUser\":\"" . $this->db->validateUser($request->user,$request->password) . "\"}";
-          break;
+            case 'post':
+                  $request = json_decode($body);
+                  header('Content-type: application/json');
+                  echo "{\"ValidUser\":\"" . $this->db->validateUser($request->user,$request->password) . "\"}";
+                  break;
 
-    default:
-          http_response_code(405);
-
-    }
-    
-
+            default:
+                  http_response_code(405);
+        }
    }
-
 }
-
-
-
+// class handles db connection   
  class dbConnect {
-    
+     // initialize class
      public function __construct($servername, $username, $password){
-          $this->servername = $servername; 
-          $this->username = $username;
-          $this->password = $password; 
-          $this->dataBase = $username;                                                           
-          // create table if it does not exist 
+         $this->servername = $servername; 
+         $this->username = $username;
+         $this->password = $password; 
+         $this->dataBase = $username;         
+                                                  
+         // create table if it does not exist           
+         $sql = "CREATE TABLE IF NOT EXISTS Users (name VARCHAR(20), password VARCHAR(100));";  
+         $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
+         $result = mysqli_query($connection,$sql); 
           
-          $sql = "CREATE TABLE IF NOT EXISTS Users (name VARCHAR(20), password VARCHAR(100));";  
-          $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
-          $result = mysqli_query($connection,$sql); 
-          
-          if(!$result){
-	      die("failed to create table");  
-          }
-
-
+         if(!$result){ die("failed to create table"); }
      }
-
-     public function getHash($password){
-
-         $retVal = password_hash($password,PASSWORD_DEFAULT);
-         return $retVal; 
-     }
-
-     public function validateUser($user,$password){
+    //validate password and return a string 
+    public function validateUser($user,$password){
         $sql = "SELECT password FROM Users WHERE name = '" . $user ."' LIMIT 1"; 
        
         $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
@@ -86,21 +62,18 @@ class httpHandler {
         if(password_verify($password,$storedPassword->password)){
            $retVal = "true"; 
         }
-
         return $retVal;    
       }
-     
 }
-
+// initialize http object 
 $http = new httpHandler("../secrets.txt"); 
 
+// take variables from request  
 $request = explode('/',$_SERVER['PATH_INFO']);
-
 $method = strtolower($_SERVER['REQUEST_METHOD']);
-
 $body = file_get_contents('php://input');
 
+// handle request 
 $http->handleRequest($request, $method, $body);
-
 
 ?>
