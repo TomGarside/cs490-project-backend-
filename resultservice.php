8a9e82e7403a58fd4096cff238502773b0960606
Thomas Garside <tg253@afsconnect1.njit.edu>
2020-02-29 19:08:24 -0500

    added result service

--- resultservice.php
+++ resultservice.php
@@ -25,19 +25,25 @@ class httpHandler {
         switch($method){
             // insert result  
             case 'post':
-                  $request = json_decode($body);
+                  $body = json_decode($body);
                   header('Content-Type: application/json');
-                  echo $this->db->createExamResult($request->name, $request->creator, $request->questions);
+                  echo $this->db->createExamResult($body->user, $body->exam, $body->results);
                   break;
             // updates result with professor updates  
             case 'put':
+                  $body = json_decode($body);
                   header('Content-Type: application/json');
-                  echo $this->db->updateExamResult();
-            // return result (combo of student and exam)  
+                  echo $this->db->updateExamResult($body->user, $body->exam, $body->question, $body->autograde, $body->adjustedGrade);
+                  break;
+            // return result (combo of student and exam) if student not specified gets all results for exam   
             case 'get':
                   header('Content-Type: application/json');
-                  echo $this->db->getExamResult($_GET["user"],$_GET["exam"]);
-
+                  if (isset($_GET["user"])){
+                      echo $this->db->getExamResult($_GET["user"], $_GET["exam"]);
+                  } else{
+                      echo $this->db->getAllExamResults( $_GET["exam"]);
+                  }
+                  break;
             default:
                   http_response_code(405);
         }
@@ -52,33 +58,52 @@ class httpHandler {
          $this->password = $password; 
          $this->dataBase = $username;        
      }
-
-    public function createExam($name, $creator, $questions){
-        $sql = "INSERT INTO exam (name,creator) VALUES (\"" . $name . "\",\"" . $creator . "\");"; 
-        foreach($questions as &$question){
-           $sql = $sql . "\nINSERT INTO examQuestion (examname, questionName) VALUES (\"" . $name . "\",\"" . $question->name . "\");";
+     //post exam results for a single student  
+    public function createExamResult($name, $exam, $results){
+        $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase); 
+        $sql=""; 
+        foreach($results as &$result){
+            $sql.= "\nINSERT INTO"; 
+            $sql.= " questionResult (user, exam, question, answer, autograde, adjustedGrade, finalGrade)";
+            $sql.= " VALUES (\"" . $name . "\",\"" . "$exam" . "\",\"" . $result->question . "\",\"" . mysqli_real_escape_string($connection,$result->answer) . "\",\"" . $result->autograde . "\",\"" . $result->adjustedGrade ."\",\"" . $result->finalGrade ."\");";    
         }
         
-        $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
         $result = mysqli_multi_query($connection,$sql);
         return mysqli_error($connection );
   
     }
-    //validate password and return a string 
-    public function getexam($examName){
-        $sql = "SELECT * FROM exam WHERE name=\"" . $examName . "\" LIMIT 1;"; 
-       
-        $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
+     // get all student results for a single exam
+     public function getAllExamResults($exam){
+        $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase); 
+        $sql = "SELECT user.name AS user, questions.name as question, questions.testCases, answer, autoGrade, adjustedGrade, finalGrade  FROM questionResult JOIN exam ON questionResult.exam=exam.name JOIN questions ON questionResult.question=questions.name JOIN user ON questionResult.User=user.name WHERE questionResult.exam = \"" . $exam . "\";"; 
         $result = mysqli_query($connection,$sql);
-        $jsonResponse = $result->fetch_assoc();        
-         
-        $sqlQuestions = "SELECT * FROM questions INNER JOIN examQuestion ON questions.name=examQuestion.questionName WHERE examQuestion.examname=\"" . $examName ."\";";
-        $questionResult = mysqli_query($connection,$sqlQuestions);
-
-        while ($row = $questionResult->fetch_assoc()) {
+        while ($row = $result->fetch_assoc()) {
+           $row["testCases"] = json_decode($row["testCases"]); 
+           $results_array[] = $row;
+        }
+        $output[$exam]=$results_array;
+        return json_encode($output); 
+     }
+    // get exam results for a sigle student 
+    public function getExamResult($user, $examName){
+        $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
+        $sql = "SELECT  questions.name as question, questions.testCases, answer, autoGrade, adjustedGrade, finalGrade  FROM questionResult JOIN exam ON questionResult.exam=exam.name JOIN questions ON questionResult.question=questions.name JOIN user ON questionResult.User=user.name WHERE questionResult.exam = \"" . $examName . "\" AND questionResult.user = \"". $user. "\";";
+        $result = mysqli_query($connection,$sql); 
+        while ($row = $result->fetch_assoc()) {
+            $row["testCases"] = json_decode($row["testCases"]);
             $results_array[] = $row;
-           }
-        return "{\"name\":\"" . $jsonResponse["name"] . "\", \"creator\":\"" . $jsonResponse["creator"] . "\", \"questions\":" . json_encode($results_array); 
+        }
+        $output["user"] = $user; 
+        $output["exam"] = $examName; 
+        $output["results"] = $results_array;
+        return json_encode($output);
+     }
+    //update exam result for student 
+     public function updateExamResult($user, $exam, $question, $autoGrade, $adjustedGrade) {
+        $connection = mysqli_connect($this->servername, $this->username, $this->password, $this->dataBase);
+        $sql = "UPDATE questionResult SET autoGrade=\"". $autoGrade . "\", adjustedGrade=\"" . $adjustedGrade . "\" WHERE User=\"" . $user . "\" AND exam=\"" . $exam . "\" AND question=\"" . $question . "\";";
+        $result = mysqli_query($connection, $sql);
+        return $result; 
     }
 }
 // initialize http object 
